50d3de0a5dc4a77925a29d232ee0596a42e71593
sainimu78 <393584307@qq.com>
2025-04-12 20:34:51 +0800

    简易测试

--- Source/NiflectHelloWorld/NiflectHelloWorld.cpp
+++ Source/NiflectHelloWorld/NiflectHelloWorld.cpp
@@ -1,8 +1,15 @@
 #include "NiflectHelloWorld.h"
 #include <stdio.h>
+#include "NiflectHelloWorld_private.h"
 
 int main()
 {
-	printf("nihao\n");
+	Niflect::CNiflectTable table;
+	Niflect::GeneratedRegisterTypes(&table);
+	Niflect::GeneratedInitTypes();
+	table.InitTypesLayout();
+
+	printf("Type name: %s\n", table.GetTypeByIndex(0)->GetTypeName().c_str());
+
 	return 0;
 }
--- Source/NiflectHelloWorld/NiflectHelloWorld.h
+++ Source/NiflectHelloWorld/NiflectHelloWorld.h
@@ -1,1 +1,8 @@
 #pragma once
+#include "NiflectHelloWorld_gen.h"
+
+NIF_T()
+class CHelloWorld
+{
+
+};
