12dddfd33423a06c32b9e1904dabc713fec3d722
sainimu78 <393584307@qq.com>
2025-04-13 00:46:34 +0800

    移除废弃命令; 更 id

--- CodespacesSetup.sh
+++ CodespacesSetup.sh
@@ -1,7 +1,3 @@
-#sudo apt update
 git submodule update --init --remote
-#sudo apt install wget unzip
 wget -O ThirdParty/Niflect/Niflect.zip "https://drive.usercontent.google.com/download?id=182fv2wlo1AKxTOH37VosCu18GICwdiHQ&export=download&authuser=0&confirm=t&uuid=b7230614-6a97-4c7a-afd0-418cd83886d3&at=APcmpoxON3sAUnH57WDvaIvUQ7Bx:1744472449906"
 wget -O ThirdParty/NiflectGenTool/NiflectGenTool.zip "https://drive.usercontent.google.com/download?id=1Q2WGtpVsj1nuqklss_NIG6U9jp2mB77U&export=download&authuser=0&confirm=t&uuid=dbb3a3a0-9b36-4a54-b4ef-33a587544caf&at=APcmpowi6Oc-i64KCatJec-N3E0f:1744472463946"
-#unzip /workspaces/NiflectHelloWorld/ThirdParty/Niflect/file1.zip -d /workspaces/NiflectHelloWorld/ThirdParty/Niflect
-#unzip /workspaces/NiflectHelloWorld/ThirdParty/NiflectGenTool/file2.zip -d /workspaces/NiflectHelloWorld/ThirdParty/NiflectGenTool
