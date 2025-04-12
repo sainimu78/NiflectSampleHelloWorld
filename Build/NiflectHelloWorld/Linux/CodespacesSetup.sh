f4065ce3647c61fd9bf9da088a64a47e79933268
sainimu78 <393584307@qq.com>
2025-04-12 23:56:10 +0800

    Codespaces 依赖

---
+++ Build/NiflectHelloWorld/Linux/CodespacesSetup.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install wget unzip
+wget -O /workspaces/NiflectHelloWorld/ThirdParty/Niflect/Niflect.zip "https://drive.usercontent.google.com/download?id=182fv2wlo1AKxTOH37VosCu18GICwdiHQ&export=download&authuser=0&confirm=t&uuid=b7230614-6a97-4c7a-afd0-418cd83886d3&at=APcmpoxON3sAUnH57WDvaIvUQ7Bx:1744472449906"
+wget -O /workspaces/NiflectHelloWorld/ThirdParty/NiflectGenTool/NiflectGenTool.zip "https://drive.usercontent.google.com/download?id=1Q2WGtpVsj1nuqklss_NIG6U9jp2mB77U&export=download&authuser=0&confirm=t&uuid=dbb3a3a0-9b36-4a54-b4ef-33a587544caf&at=APcmpowi6Oc-i64KCatJec-N3E0f:1744472463946"
+#unzip /workspaces/NiflectHelloWorld/ThirdParty/Niflect/file1.zip -d /workspaces/NiflectHelloWorld/ThirdParty/Niflect
+#unzip /workspaces/NiflectHelloWorld/ThirdParty/NiflectGenTool/file2.zip -d /workspaces/NiflectHelloWorld/ThirdParty/NiflectGenTool
