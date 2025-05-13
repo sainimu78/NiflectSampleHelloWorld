6d951e6f2c0c99695523f8dcde3ce174339a6b37
sainimu78 <393584307@qq.com>
2025-05-14 02:38:22 +0800

    删除多余初始化, 非必要 include

--- Source/HelloWorld/HelloWorld.h
+++ Source/HelloWorld/HelloWorld.h
@@ -1,15 +1,9 @@
 #pragma once
-#include "HelloWorld_gen.h"
+#include "Niflect/Default/DefaultMacroTag.h"
 
 NIF_T()
 class CHelloWorld
 {
-public:
-	CHelloWorld()
-		: m_value(0.0f)
-	{
-	}
-
 public:
 	NIF_F()
 	float m_value = 0.0f;
