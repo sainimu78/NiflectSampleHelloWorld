8de97ee08bc3e2a9246f699acead96722270cbd3
sainimu78 <393584307@qq.com>
2025-06-14 17:27:05 +0800

    简化演示, 改用 reg

--- Source/HelloWorld/Main.cpp
+++ Source/HelloWorld/Main.cpp
@@ -2,6 +2,8 @@
 #include "Niflect/Serialization/JsonFormat.h"
 #include "HelloWorld_private.h"
 
+using namespace RwTree;
+
 static void InitForTest(CHelloWorld& instance)
 {
 	instance.m_value = 1.23f;
@@ -14,43 +16,27 @@ static bool operator==(const CHelloWorld& lhs, const CHelloWorld& rhs)
 
 int main()
 {
-	//Container for registered types
-	Niflect::CNiflectTable table;
+	//Initialize types registry
+	Niflect::CNiflectModuleRegistry2 reg;
+	reg.InitLoadTimeModules();
 
-	//Initialize types in stages
-	{
-		Niflect::GeneratedRegisterTypes(&table);
-		Niflect::GeneratedInitTypes();
-		table.BuildTypesMeta();
-	}
+	auto type = Niflect::StaticGetType<CHelloWorld>();
+	printf("Registered type name: %s\n", type->GetTypeName().c_str());
 
-	//Verify initialization succeeded
-	{
-		auto type = Niflect::StaticGetType<CHelloWorld>();
-		printf("Registered type name: %s\n", type->GetTypeName().c_str());
-	}
+	//Save src to rw
+	CRwNode rw;
+	CHelloWorld src;
+	InitForTest(src);
+	type->SaveInstanceToRwNode(&src, &rw);
+	//Load dst from rw
+	CHelloWorld dst;
+	type->LoadInstanceFromRwNode(&dst, &rw);
+	assert(src == dst);
 
-	//Saving, loading and serialization
-	{
-		using namespace RwTree;
-		Niflect::CStringStream ss;
-		{
-			CRwNode rwDst;
-			{
-				CRwNode rwSrc;
-				CHelloWorld src;
-				CHelloWorld dst;
-				InitForTest(src);
-				auto type = Niflect::StaticGetType<CHelloWorld>();
-				type->SaveInstanceToRwNode(&src, &rwSrc);
-				type->LoadInstanceFromRwNode(&dst, &rwSrc);
-				assert(src == dst);
-				type->SaveInstanceToRwNode(&dst, &rwDst);
-			}
-			CJsonFormat::Write(&rwDst, ss);
-		}
-		printf("%s\n", ss.str().c_str());
-	}
+	//Serialize rw in JSON
+	Niflect::CStringStream ss;
+	CJsonFormat::Write(&rw, ss);
+	printf("%s\n", ss.str().c_str());
 
 	return 0;
 }
